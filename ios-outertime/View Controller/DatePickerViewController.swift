3ad0fea37cdfd68411bf8740bcba90ff8d0937e9
iyinraphael <iyinraphael@gmail.com>
2020-04-28 20:14:22 -0700

    initial set uip

---
+++ ios-outertime/View Controller/DatePickerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DatePickerViewController.swift
+//  ios-outertime
+//
+//  Created by Iyin Raphael on 4/28/20.
+//  Copyright © 2020 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    
+    
+}
---
+++ ios-outertime/View Controller/TimeCircuitsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TimeCircuitsViewController.swift
+//  ios-outertime
+//
+//  Created by Iyin Raphael on 4/28/20.
+//  Copyright © 2020 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TimeCircuitViewController: UIViewController {
+    
+}
